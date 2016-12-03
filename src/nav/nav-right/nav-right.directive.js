78968e71322f0490933ed69fb20c91ccc8ecd6c0
benitogf <benitogf@outlook.com>
2016-12-03 14:21:17 +0800

    hint fix

--- src/nav/nav-right/nav-right.directive.js
+++ src/nav/nav-right/nav-right.directive.js
@@ -17,23 +17,9 @@ function NavRightController ($scope, $mdSidenav, i18nService, $translate) {
   var vm = this
   vm.languages = i18nService.languages
   vm.selectedLang = $translate.use()
-  //vm.prevLang = $translate.use()
-  //vm.savePreferences = savePreferences
   vm.changeLang = changeLang
-  //vm.close = close
 
   function changeLang () {
     $translate.use(vm.selectedLang)
   }
-
-  // function savePreferences () {
-  //   close().then(function () {
-  //     $translate.use(vm.selectedLang)
-  //   })
-  // }
-  //
-  // function close () {
-  //   $translate.use($scope.lang)
-  //   return $mdSidenav('right').close()
-  // }
 }
